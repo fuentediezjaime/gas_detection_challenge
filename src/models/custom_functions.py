2df8e73a125eade209e0a5deb01a5ce202faa63b
jaime <fuentediezjaime>
2025-04-04 00:11:03 +0200

    Added basic tree implementation to test. Did PCA on the feature blocks

--- src/models/custom_functions.py
+++ src/models/custom_functions.py
@@ -1,19 +1,23 @@
 import numpy as np
-
-def loss_custom(y_pred, y_real):
-
-    #Ensuring np array:
-    y_pred = np.array(y_pred)
-    y_real = np.array(y_real)
-
-    # Vector of ponderation coefs to punish more errors at cases where there is an alarm
-    ponderation = np.where(y_real >= 0.5, 1.2, 1.0)
-
-    # Applying ponderation and computing square root error
-    sq_errs = ponderation * (y_pred-y_real)**2
-    mean_samples = np.mean(sq_errs, axis=1)
-    mean_all_sqrt = np.sqrt(np.mean(mean_samples))
-    return mean_all_sqrt
-import numpy as np
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import make_scorer
 
 
+def loss_custom(y_real, y_pred):
+
+    if (y_real >= 0.5):
+        error = 1.2*(y_real-y_pred)**2
+    else: 
+        error = 1*(y_real-y_pred)**2
+
+
+def hyperpar_search(model, param_dict, n_cv):
+
+    scorer = make_scorer(loss_custom,greater_is_better=False)
+    # First, perfomr a random search
+    rand_search = RandomizedSearchCV(estimator=model, 
+                                     param_distributions=param_dict, 
+                                     scoring=None, 
+                                     cv=n_cv,
+                                     n_iter=10)
+    return rand_search
