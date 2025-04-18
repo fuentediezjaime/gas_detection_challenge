8dfe8205f47b484dde537ee04f18c376a1997f78
jaime <fuentediezjaime>
2025-04-18 15:24:14 +0200

    efer

---
+++ src/features/setup_collab.py
@@ -0,0 +1,5 @@
+def configure_collab_gpu()
+    '''
+    This function executes commands in the jupyter terminal such that the environment is configured to run gpu-enabled 
+    libraries when it is possible (for example, to set light-gbm to run in gpu environments) 
+    '''
